82684dc7ec6ed2303a9b37317bc45f8a1aee8b05
Sukhvinder Singh <46042060+Sukhvsin2@users.noreply.github.com>
2022-09-28 20:45:08 -0500

    Efficient solution

--- moveZero.cpp
+++ moveZero.cpp
@@ -1,30 +1,31 @@
+// class Solution {
+// public:
+//     void moveZeroes(vector<int>& nums) {
+//         vector<int> res, zero;
+//         for(int i=0;i<nums.size();i++)
+//             if(nums[i] == 0) zero.push_back(nums[i]);
+        
+//         for(int i=0;i<nums.size();i++)
+//             if(nums[i] != 0)
+//                 res.push_back(nums[i]);
+        
+//         for(int i=0;i<res.size();i++)
+//             nums[i] = res[i];
+        
+        
+//         for(int i=res.size(), j = 0;i<res.size()+zero.size();i++, j++)
+//             nums[i] = zero[j];
+//     }
+// };
 class Solution {
 public:
-    // void swap(int &a, int &b){
-    //     int temp = a;
-    //     a = b;
-    //     b = temp;
-    // }
-    // void printArr(vector<int> arr){
-    //     for(auto i: arr)
-    //         cout<<i<<" ";
-    //     cout<<endl;
-    // }
-    
     void moveZeroes(vector<int>& nums) {
-        vector<int> res, zero;
-        for(int i=0;i<nums.size();i++)
-            if(nums[i] == 0) zero.push_back(nums[i]);
-        
-        for(int i=0;i<nums.size();i++)
-            if(nums[i] != 0)
-                res.push_back(nums[i]);
-        
-        for(int i=0;i<res.size();i++)
-            nums[i] = res[i];
-        
-        
-        for(int i=res.size(), j = 0;i<res.size()+zero.size();i++, j++)
-            nums[i] = zero[j];
+        int index = 0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i] != 0){
+                swap(nums[i], nums[index]);
+                index++;
+            }
+        }
     }
 };

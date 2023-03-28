9075337a539b16ba23197581c84fc8b81582e7bf
Sukhvinder Singh <46042060+Sukhvsin2@users.noreply.github.com>
2023-03-28 09:49:47 -0400

    Create twoSum.cpp

---
+++ daily/100daycode/twoSum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> m;
+        vector<int> res;
+        for(int i=0;i<nums.size();i++){
+            if(m.find(target-nums[i]) != m.end()){
+                res.push_back(m.find(target-nums[i])->second);
+                res.push_back(i);
+            }
+            m[nums[i]] = i;
+        }
+        return res;
+    }
+};
